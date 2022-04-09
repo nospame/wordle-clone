2c55e482606f802440505d418f2542251068fca0
nospame <mapson.evan@gmail.com>
2022-04-09 10:12:43 -0700

    add basic guess input with correct/incorrect feedback

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,52 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+<!-- 
+There is a correct word
+Allow user to enter a word
+Provide feedback as to whether that word is correct
+Guess must be five letters long
+
+Allow user to enter more guesses (up to six)
+Provide feedback as to whether each letter is correct
+
+Only allow guesses that are actually words (API call)
+
+
+ -->
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      message: "Wordle",
+      correct: "PRANK",
+      guess: '',
+      response: ''
+    };
+  },
+  created: function () { },
+  methods: {
+    checkGuess: function () {
+      if (this.guess.length != 5) {
+        this.response = 'Guess must be five letters.'
+      } else if (this.guess.toUpperCase() != this.correct) {
+        this.response = 'Incorrect.'
+      } else {
+        this.response = 'You got it!'
+      }
+    },
   }
 }
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <!-- <div v-for="guess in guesses">
+      <h2>{{ guess }}</h2>
+    </div>-->
+    <p>{{ response }}</p>
+    <input v-model="guess" />
+    <button v-on:click="checkGuess()">Enter</button>
+  </div>
+</template>
+
+<style></style>
