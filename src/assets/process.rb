861aaa5858f3f1bfcab64ff8152fa4da93286fe0
nospame <mapson.evan@gmail.com>
2022-05-23 18:46:29 -0700

    remove ruby script to process csv dataset

--- src/assets/process.rb
+++
@@ -1,9 +0,0 @@
-require 'csv'
-
-lines = CSV.parse(File.read('valid_guesses.csv'), headers: true)
-lines_array = []
-lines.each do |line|
-  lines_array << line["word"]
-end
-
-File.write('guess.json', lines_array)
